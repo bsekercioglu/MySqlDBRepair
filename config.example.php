2254982fb47429d610a257d06c614a1216264821
Burak ŞEKERCİOĞLU <burak@sekercioglu.eu>
2025-12-27 17:00:18 +0300

    config.example.php dosyası eklendi

---
+++ config.example.php
@@ -0,0 +1,10 @@
+<?php
+// Veritabanı bağlantı ayarları
+// Bu dosyayı config.php olarak kopyalayın ve kendi MySQL bilgilerinizi yazın
+
+return [
+    'host' => 'localhost',
+    'dbname' => 'kurs',
+    'username' => 'root',
+    'password' => '',
+];
